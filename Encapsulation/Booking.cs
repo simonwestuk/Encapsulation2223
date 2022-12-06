22e52b26b000fc22f1c5403d679ff2195156c681
wests <wests@C-010053>
2022-12-06 12:16:06 +0000

    Commit

---
+++ Encapsulation/Booking.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Booking
+    {
+        public Guid Id { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public string Type { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public Booking(Vehicle vehicle, string type, DateTime timestamp)
+        {
+            Id = Guid.NewGuid();
+            Vehicle = vehicle;
+            Type = type;
+            Timestamp = timestamp;
+        }
+
+        public override string ToString()
+        {
+            return $"Booking ID: { Id }\nVehicle: { Vehicle }\nType:{ Type }\nDate and Time:{ Timestamp }";
+        }
+    }
+}
---
+++ Encapsulation/Garage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Garage
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Booking> Bookings { get; set; }
+
+        public Garage(string name, string address)
+        {
+            Name = name;
+            Address = address;
+            Bookings = new List<Booking>();
+        }
+    }
+}
